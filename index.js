7676d1a4798e87200383390e8a957d4ee5e119f2
Rubberduckduck <bryanlim805@gmail.com>
2025-10-25 18:30:04 +0800

    Post and Get comamnds done
    TODO: Delete, Get with ID, Put, Patch

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 // This is equivalent to import express, in cpp way, is bringinig in the dependency
 const express = require('express');
 const app = express(); 
+const crypto = require('crypto');
 // Port is the endpoint for communication between the 2 computers
 const PORT = 8888;
 
 // Middleware, convert body to json
 app.use(express.json());
 
+// Hard code the start todo for now
+let todos = []
+
 // This will in turn: GET http://localhost:<PORT>/one-piece
 // GET is to retrieve data from a server
 // 2nd arg is callback function
-app.get('/one-piece', 
+app.get('/todos', 
     // When this exact url fires, this function will fire to handle the request
     (req, res)=> {
-        res.status(200).send({
+        res.status(200).json({
             // This will be in a json format
-            character: 'monkey D. luffy',
-            job: 'pirate'
-        })
+            // To read all todos
+            Message: "Read Todos",
+            Count: todos.length,
+            data: todos,
+        });
     } 
 );
 
 // Post: Create new data
-app.post('/one-piece/:id', 
+app.post('/todos', 
     // When this exact url fires, this function will fire to handle the request
     (req, res)=> {
-            const{id} = req.params;
-            const{crew_name} = req.body;
+        
+        // Get data from request's body
+        const { task, description } = req.body = req.body;
 
-            // Check if have right_hand
-            if(!crew_name){
-                return res.status(400).json({message: 'Pirate needs a crew'});
-            }
-
-            // Success response with 201 status
-            res.status(201).json({
-                message: 'Crew member added successfully',
-                crew: `pirate in the ${crew_name} and ID of ${id}`,
+        // Validate input
+        if(!task || task.trim() === ""){
+            return res.status(400).json({
+                message: "Tasks is required in order to be added",
             });
+        }
+
+        // Create new todo
+        const new_todo = {
+            // Randomize GUID
+            ID: crypto.randomUUID(),
+            task: task.trim(),
+            description: description || "",
+            completed: false,
+            created_at: new Date().toISOString()
+        }
+
+        todos.push(new_todo);
+
+
+        // Success response with 201 status
+        res.status(201).json({
+            message: "Todo added successfully",
+            data: new_todo
+        });
     } 
 );
 
